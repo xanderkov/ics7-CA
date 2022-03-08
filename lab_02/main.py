64bf98be26b45864f464217958e296cef5ba5ee9
Kovel Alexander <xanderkovel@gmail.com>
2022-03-08 16:08:25 +0300

    Added coef check

--- lab_02/main.py
+++ lab_02/main.py
@@ -4,7 +4,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import re
+from mpl_toolkits import mplot3d
 
 
 def get_polynomial_degree():
@@ -48,19 +48,50 @@ def get_data(linesz, linesx, linesy):
     return nnnum, linesy, linesz
 
 
-def create_n_table(z, y, x, nx, ny, nz):
+def getNearCoef(arr, xMean):
+    for i in range(len(arr)):
+        if arr[i] > xMean:
+            return i
+
+def getFirstColumnTable(arr, n, coef):
+    start = 0
+    end = 0
+    if (coef - n // 2 >= 0):
+        start = coef - n// 2
+    if (start == 0):
+        end = n + 1
+    else:
+        end = coef + n // 2 + n % 2 + 1
+        if (end > n):
+            start -= (end - n - 2) 
+            end = n + 1
+    arr_copy = []
+    #print(coef, start, end)
+    for i in range(start, end):
+        arr_copy.append(arr[i])
+    
+    return arr_copy
+
+
+def create_n_table(z, y, x, nx, ny, nz, xMean, yMean, zMean):
     table = np.zeros([nz + 1, ny + 1, nx + 1])
     for i in range(nz + 1):
         for j in range(ny + 1):
+            coef = getNearCoef(x[i][j], xMean)
+            arr = getFirstColumnTable(x[i][j], nx + 1, coef)
             for k in range(nx + 1):
-                table[i][j][k] = x[i][j][k]
+                table[i][j][k] = arr[k]
     linesy = np.zeros([nz + 1, ny + 1])
     for i in range(nz + 1):
+        coef = getNearCoef(y[i], yMean)
+        arr = getFirstColumnTable(y[i], ny + 1, coef)
         for j in range(ny + 1):
-            linesy[i][j] = y[i][j]
+            linesy[i][j] = arr[j]
     linesz = np.zeros(nz + 1)
+    coef = getNearCoef(z, zMean)
+    arr = getFirstColumnTable(z, nz + 1, coef)
     for i in range(nz + 1):
-        linesz[i] = z[i]
+        linesz[i] = arr[i]
     return table, linesy, linesz
 
 
@@ -83,6 +114,9 @@ def getNewtonPoly(table, x, xMean, n):
     return yx
 
 def get_answer(x, z, y, nx, ny, nz, xMean, yMean, zMean):
+    #print(x)
+    #print(y)
+    #print(z)
     yInter = np.zeros([nz + 1, ny + 1])
     for i in range(0, nz + 1):
         for j in range(0 , ny + 1):
@@ -93,7 +127,7 @@ def get_answer(x, z, y, nx, ny, nz, xMean, yMean, zMean):
     for i in range(nz + 1):
         table = getDivededDiff(y[i], yInter[i], ny + 1)[0, :]
         zInter.append(getNewtonPoly(table, y[i], yMean, ny + 1))
-    print(z, zInter)
+    print(zInter)
     table = getDivededDiff(z, zInter, nz + 1)[0, :]
     res = getNewtonPoly(table, z, zMean, nz + 1)
     return res
@@ -101,8 +135,8 @@ def get_answer(x, z, y, nx, ny, nz, xMean, yMean, zMean):
 
 if __name__ == '__main__':
     nx, ny, nz = get_polynomial_degree()
-    xMean, yMean, zMean = map(int, input("Input x, y, z: ").split())
+    xMean, yMean, zMean = map(float, input("Input x, y, z: ").split())
     linesz, linesx, linesy = [], [], []
     table, linesy, linesz = get_data(linesz, linesx, linesy)
-    table, linesy, linesz = create_n_table(linesz, linesy, table, nx, ny, nz)
+    table, linesy, linesz = create_n_table(linesz, linesy, table, nx, ny, nz, xMean, yMean, zMean)
     print(get_answer(table, linesz, linesy, nx, ny, nz, xMean, yMean, zMean))

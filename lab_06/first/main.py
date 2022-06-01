0f9d4b01ca4f4709fa98fdd2ac7583a5431f77e9
Kovel Alexadner <xanderkov@gmail.com>
2022-06-01 13:18:15 +0300

    No changes

--- lab_06/first/main.py
+++ lab_06/first/main.py
@@ -4,17 +4,17 @@ from prettytable import PrettyTable
 from gauss import *
 from function import *
 
-N_MAX = 15
-M_MAX = 15
+N_MAX = 12
+M_MAX = 12
 
 x_left = 0
 x_right = 2
 y_bottom = -1
 y_top = 1
-table = PrettyTable(['n\m'] + list(range(2, M_MAX)))
+table = PrettyTable(['Кол-во узлов'] + list(range(2, M_MAX)))
 for n in range(2, N_MAX):
     string = [n]
     for m in range(2, M_MAX):
-        string.append(f'{gauss_integ(n, m, x_left, x_right, y_bottom, y_top, f):5.5f}')
+        string.append(f'{gauss_integ(n, m, x_left, x_right, y_bottom, y_top, f):f}')
     table.add_row(string)
 print(table)
